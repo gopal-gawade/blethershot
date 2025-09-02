313b6a49293666cbf8b0c353cb3b07fd9feca859
gopal-gawade <gawadegopal1211@gmail.com>
2025-09-02 14:41:37 +0530

    build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
+import Scroll from "../components/Scroll/Scroll";
 import Navbar from "../components/Navbar/Navbar";
-import { ClerkProvider } from "@clerk/nextjs";
 import Footer from "../components/Footer/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "Blethershot",
@@ -24,6 +25,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${open_sans.className}`}>
+          <Scroll/>
+
           <nav className="w-full bg-white/70 backdrop-blur-xl border-b border-[#e5e7eb] shadow-[0_8px_24px_rgba(0,0,0,0.06)] px-2 md:px-4 lg:px-8 xl:px-16 2xl:px-32 fixed z-20">
             <Navbar />
           </nav>
